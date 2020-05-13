9b90db241992c76f70eaf667c0c34ef92c3f0b6e
Annapoorni Ravi <AnnapoorniRavi@users.noreply.github.com>
2020-05-13 00:00:55 -0500

    Add files via upload

---
+++ Coursera_Week_1.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# _Coursera Capstone Project_¶
+# Week1
+# Create Jupyter Notebook for Capstone project work
+# 
+# Week 1 - Assignment Details
+# Write some markdown to explain that this notebook will be mainly used for the capstone project.
+# Import the pandas library as pd.
+# Import the Numpy library as np.
+# Print the following the statement: Hello Capstone Project Course!
+
+# In[ ]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[1]:
+
+
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
